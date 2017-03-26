d997b0d6f14c0ebde49c3ab49c6a80ba5743cbbe
Matt Moore <moorem27@uw.edu>
2017-03-25 22:12:03 -0700

    Attempt to use wiringPi

--- client.cc
+++ client.cc
@@ -8,8 +8,15 @@
 #include <stdlib.h>
 #include <thread>
 #include <chrono>
+#include <wiringPi.h>
 //raspistill -o /home/pi/samsung/first_pic2.jpg
-const char* take_video = "raspivid -o /home/pi/samsung/video.h264 -t 10000 -d";
+
+namespace {
+	const char* take_video = "raspivid -o /home/pi/samsung/video.h264 -t 30000 -d";
+	const char* remove_video = "rm /home/pi/samsung/video.h264";
+	const std::string path = "/home/pi/samsung/video.h264";
+	const int send_size = 163840;
+}
 
 int create_connection() {
 	struct sockaddr_in server_connection{};
@@ -26,26 +33,30 @@ int create_connection() {
 		}
 	return file_descriptor;
 } 	
-		
-int main( void ) {
-	// 163840  (/proc/sys/net/core/rmem_max)
- 	system( take_video );
-	std::string path = "/home/pi/samsung/video.h264";
-	int send_fd = create_connection();
-	int send_size = 163840; 
-	setsockopt( send_fd, SOL_SOCKET, SO_SNDBUF, &send_size, sizeof( send_size ) );
-	std::ifstream file( path.c_str(), std::ios_base::binary | std::ios::ate );
-	std::cout << "Made file" << std::endl;
-	file.seekg( 0, std::ios::beg );
-	std::vector<char> buffer( send_size,0 );
-	std::cout << "Made the vector" << std::endl;
-	
-	while( file.read( buffer.data(), buffer.size() ) ) {
-		if( send( send_fd, static_cast<void *>( buffer.data() ), buffer.size(), 0 ) < 0 )
-			std::cout << "Send failed" << std::endl;
-	}
 
-	system( "rm /home/pi/samsung/video.h264" );
+
+int main( void ) {
+	system( "gpio edge 11 falling" );
+	std::cout << wiringPiSetupGpio() << std::endl;
+	std::cout << wpiPinToGpio( 11 ) << std::endl;
+	std::this_thread::sleep_for( std::chrono::seconds( 3 ) );
+	int send_fd = create_connection();
+	std::vector<char> buffer( send_size, 0 );
+	int interrupted = waitForInterrupt( 11, -1 );
+	if( interrupted ) {
+		std::cout << "Motion detected" << std::endl;
+		system( take_video );
+		setsockopt( send_fd, SOL_SOCKET, SO_SNDBUF, &send_size, sizeof( send_size ) );
+		std::ifstream file( path.c_str(), std::ios_base::binary | std::ios::ate );
+		file.seekg( 0, std::ios::beg );
+
+		while( file.read( buffer.data(), buffer.size() ) ) {
+			if( send( send_fd, static_cast<void *>( buffer.data() ), buffer.size(), 0 ) < 0 )
+				std::cout << "Send failed" << std::endl;
+		}
+		file.close();
+		system( remove_video );
+	}
 	return 0;
 }
 
