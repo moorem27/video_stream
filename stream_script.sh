a70f8a2580303415ea477b22f20f054860e2595d
Matt <moorem27@uw.edu>
2017-05-16 06:44:30 +0000

    Add comment to stream script

--- stream_script.sh
+++ stream_script.sh
@@ -7,3 +7,6 @@ raspivid -o - -t 0 -hf -w 640 -h 360 -fps 25 | cvlc -vvv stream:///dev/stdin --s
 
 # To watch this stream on server side open VLC, press Media -> Open Networkstream and type:
 # rtsp://IP.TO.THE.PI:8554/
+# OR
+# in the command line type:
+# vlc rtsp://IP.TO.THE.PI:8554/
