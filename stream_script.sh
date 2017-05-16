c2ae810fe9504171a1111464e89bd4a191be669a
Matt <moorem27@uw.edu>
2017-05-16 06:36:40 +0000

    Add video stream example script

---
+++ stream_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Example of streaming video from raspberry pi to server using a combination of VLC and raspivid
+# VLC must be installed on Pi and server
+
+raspivid -o - -t 0 -hf -w 640 -h 360 -fps 25 | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554}' :demux=h264
+
+# To watch this stream on server side open VLC, press Media -> Open Networkstream and type:
+# rtsp://IP.TO.THE.PI:8554/
