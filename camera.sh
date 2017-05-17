0a362a1781cf43ffe69fdeaf356766b4670b40e5
Matt <moorem27@uw.edu>
2017-05-17 02:28:16 +0000

    Rename script for camera

--- camera.sh
+++ camera.sh
@@ -5,10 +5,3 @@
 
 raspivid -o - -t 0 -hf -w 640 -h 360 -fps 25 | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554}' :demux=h264
 
-# To watch this stream on server side open VLC, press Media -> Open Networkstream and type:
-# rtsp://IP.TO.THE.PI:8554/
-# OR
-# in the command line type:
-# vlc rtsp://IP.TO.THE.PI:8554/
-# for plain interface type:
-# cvlc rtsp://IP.TO.THE.PI:8554/
